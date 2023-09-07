bc6f094639ad9e5cfaa3e0430269b522134fabca
skndrali22 <skndrshaikh17c@gmail.com>
2023-09-07 15:20:45 +0500

    features removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,16 +37,6 @@ export default function Navbar(props) {
                 {props.aboutText}
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                {props.aboutText}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                {props.aboutText}
-              </Link>
-            </li>
             
             {/* <li className="nav-item">
               <a className="nav-link" href="#">
