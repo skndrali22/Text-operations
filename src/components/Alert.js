46d7298ec1408dbeb42b23e7572b22e4f5eba70f
skndrali22 <skndrshaikh17c@gmail.com>
2023-09-18 11:18:42 +0500

    new update

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -7,10 +7,14 @@ export default function Alert(props) {
     }
   return (
     
-        props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-        <strong>{capitalized( props.alert.type)}</strong>:{props.alert.msg}
-        
+      <div style={{height:'60px'}}>
+        {
+          props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+          <strong>{capitalized( props.alert.type)}</strong>:{props.alert.msg}
+        </div>
+        }
       </div>
+        
     
   );
 }
